e982b90ea59ef28518ffdadf7bd6e4c48e67d551
Jsinco <jonah@jsinco.dev>
2026-07-18 09:36:52 -0400

    fix(api): PointedDripstoneConfiguration wireprovider

--- api/src/main/java/dev/wyck/worldgen/feature/configurations/PointedDripstoneConfiguration.java
+++ api/src/main/java/dev/wyck/worldgen/feature/configurations/PointedDripstoneConfiguration.java
@@ -20,7 +20,7 @@ import org.jspecify.annotations.NullMarked;
 public interface PointedDripstoneConfiguration extends FeatureConfiguration {
 
     @ApiStatus.Internal
-    ConstructWireProvider<PointedDripstoneConfiguration> WIRE = ConstructWireProvider.create("dev.wyck.*?.worldgen.feature.config.PointedDripstoneConfigurationImpl");
+    ConstructWireProvider<PointedDripstoneConfiguration> WIRE = ConstructWireProvider.create("dev.wyck.*?.worldgen.feature.configurations.PointedDripstoneConfigurationImpl");
 
     /**
      * Probability for double-block dripstone between 0.0 and 1.0.
